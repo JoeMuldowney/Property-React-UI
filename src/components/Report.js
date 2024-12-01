521d6719a935daddb6fb31876dadd13ad6784bd1
JoeMuldowney <muldowneyjoe84@gmail.com>
2024-12-01 11:42:48 -0500

    adding update and report features

---
+++ src/components/Report.js
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import Styles from './Styles.module.css';
+import Container from '@mui/material/Container';
+import { Paper, Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+
+export default function Report() {
+    
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const[id,setId] = React.useState('')
+    const[firstName,setFirstName] = React.useState('')
+    const[lastName,setLastName] = React.useState('')
+    const[phoneNumber, setPhoneNumber] = React.useState('')
+    const[email, setEmail] = React.useState('')
+    const[street,setStreet] = React.useState('')
+    const[city,setCity] = React.useState('')
+    const[zipCode,setZipCode] = React.useState('')
+    const[state,setState] = React.useState('')
+    const[minPrice, setMinPrice] = React.useState('')
+    const[maxPrice, setMaxPrice] = React.useState('')
+
+    
+    const lookupAllClick=(e)=>{
+      e.preventDefault();
+      e.stopPropagation();   
+          
+     
+
+
+  };
+    
+
+
+return(
+  <Container component="form"
+    sx={{
+      '& > :not(style)': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+<Paper elevation={3} style={paperStyle}>
+            <h1 style={{color:"blue"}}><b>Report Generator</b></h1>
+            <h3 style={{color:"blue"}}><b>Select Parameters</b></h3>
+           
+         
+          <div>
+            <div className={Styles.spacing}>
+              <TextField
+                label="Search by ID"
+                variant="outlined"
+                fullWidth
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+            </div>
+
+          </div>
+          <div className={Styles.spacing}>
+              <TextField                
+                label="Last Name"
+                variant="outlined"
+                type="string"
+                fullWidth
+                placeholder={"set last name"}
+
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+      
+          
+          <div>
+            
+
+            <div className={Styles.spacing}>
+              <TextField
+                label="City"
+                variant="outlined"
+                type="string"
+                fullWidth
+                placeholder={"set city"}
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+            <div className={Styles.spacing}>
+              <TextField                
+                label="State"
+                variant="outlined"
+                type="string"
+                fullWidth
+                placeholder={"set state"}
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+            </div>
+            <div className={Styles.spacing}>
+          <TextField
+            label="Min Price"
+            type="number"
+            variant="outlined"
+            fullWidth
+            placeholder={"set min price"}
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+          />
+        </div>
+        <div className={Styles.spacing}>
+          <TextField
+            label="Max Price"
+            type="number"
+            variant="outlined"
+            fullWidth
+            placeholder={"set max price"}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+          />
+        </div>
+
+              <div className={Styles.buttonContainer}>
+              <Button variant="contained" >
+                Run
+              </Button>
+            </div>
+            </div>
+          
+      
+      </Paper>
+
+</Container>
+    
+
+);
+}
---
+++ src/components/UpdateClient.js
@@ -0,0 +1,179 @@
+import * as React from 'react';
+import Styles from './Styles.module.css';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import { Paper, Button } from '@mui/material';
+
+export default function UpdateClient() {
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const[firstName,setFirstName] = React.useState('')
+    const[lastName,setLastName] = React.useState('')
+    const[phoneNumber, setPhoneNumber] = React.useState('')
+    const[email, setEmail] = React.useState('')
+
+    const[id,setId] = React.useState('')
+    const [personData, setPersonData] = React.useState(null);
+    const [isSearched, setIsSearched] = React.useState(false);
+
+    
+    const handleClick=(e)=>{
+        e.preventDefault();
+        e.stopPropagation();   
+    const person = { firstName, lastName, phoneNumber, email }; 
+    console.log(person)
+    const idValue = id;  
+    fetch("https://www.csportfoliojm.com/backend/person/" + idValue,{
+         method: "PATCH", 
+         headers:{"Content-Type":"application/json"}, 
+         body:JSON.stringify(person)
+        })
+        .then(()=>console.log("New person added"))
+        .catch(error => {
+          console.error("Error looking up person:", error);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            console.error("Server responded with status:", error.response.status);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error("No response received:", error.request);
+          } else {
+            // Something else happened while setting up the request
+            console.error("Error setting up request:", error.message);
+          }
+    });
+  };
+  
+  const lookupClick=(e)=>{
+      e.preventDefault();
+      e.stopPropagation();   
+      const idValue = id;     
+     
+      
+     fetch("https://www.csportfoliojm.com/backend/person/"  + idValue,{
+       method: "GET", 
+       headers:{"Content-Type":"application/json"}, 
+      
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+          console.log("Person Data", data);
+          setPersonData({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            phoneNumber: data.phoneNumber,
+            email: data.email
+            
+      }
+    )
+    setIsSearched(true)})
+      .catch(error => {
+          console.error("Error looking up person:", error);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            console.error("Server responded with status:", error.response.status);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error("No response received:", error.request);
+          } else {
+            // Something else happened while setting up the request
+            console.error("Error setting up request:", error.message);
+          }
+        });
+
+  };    
+     
+  return (
+    
+    <Container
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+        <Paper elevation={3} style={paperStyle}>
+            <h1 style={{color:"blue"}}><b>Update Client</b></h1>
+            {!isSearched ? (
+          // Search Section
+          <div>
+            <div className={Styles.spacing}>
+              <TextField
+                label="Search by ID"
+                variant="outlined"
+                fullWidth
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+            </div>
+            <div className={Styles.buttonContainer}>
+              <Button variant="contained" onClick={lookupClick}>
+                Search
+              </Button>
+            </div>
+          </div>
+        ) : (
+          // Update Form
+          <div>
+            <div className={Styles.spacing}>
+              <TextField
+                
+                variant="outlined"
+                fullWidth
+                placeholder={personData.firstName || "first name"}
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className={Styles.spacing}>
+              <TextField
+                
+                variant="outlined"
+                fullWidth
+                placeholder={personData.lastName || "last name"}
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div className={Styles.spacing}>
+              <TextField
+                
+                variant="outlined"
+                fullWidth
+                placeholder={personData.phoneNumber || "phone number"}
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div className={Styles.spacing}>
+              <TextField
+                
+                variant="outlined"
+                fullWidth
+                placeholder={personData.email || "email"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className={Styles.buttonContainer}>
+              <Button variant="contained" onClick={handleClick}>
+                Update
+              </Button>
+              </div>
+              <div className={Styles.buttonContainer}>
+              <Button variant="contained" onClick={() => setIsSearched(false)} >
+                New Search
+              </Button>
+            </div>
+            </div>
+          
+        )}
+      </Paper>
+    </Container>
+  );
+}
